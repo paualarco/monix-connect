8503f90a939f76453bd49e5a83fc98c96934930d
Pau <pau.alarcon.b@gmail.com>
2020-12-19 17:56:22 +0100

    Removed index blocks padding and font changes

---
+++ website/.docusaurus/docusaurus.config.js
@@ -0,0 +1,240 @@
+export default {
+  "title": "Monix Connect",
+  "tagline": "A set of connectors and stream integrations for Monix.",
+  "url": "https://connect.monix.io",
+  "baseUrl": "/",
+  "projectName": "monix-connect",
+  "organizationName": "monix",
+  "favicon": "img/monix-logo.png",
+  "themeConfig": {
+    "sidebarCollapsible": false,
+    "prism": {
+      "additionalLanguages": [
+        "scala"
+      ],
+      "theme": {
+        "plain": {
+          "color": "#403f53",
+          "backgroundColor": "#FBFBFB"
+        },
+        "styles": [
+          {
+            "types": [
+              "changed"
+            ],
+            "style": {
+              "color": "rgb(162, 191, 252)",
+              "fontStyle": "italic"
+            }
+          },
+          {
+            "types": [
+              "deleted"
+            ],
+            "style": {
+              "color": "rgba(239, 83, 80, 0.56)",
+              "fontStyle": "italic"
+            }
+          },
+          {
+            "types": [
+              "inserted",
+              "attr-name"
+            ],
+            "style": {
+              "color": "rgb(72, 118, 214)",
+              "fontStyle": "italic"
+            }
+          },
+          {
+            "types": [
+              "comment"
+            ],
+            "style": {
+              "color": "rgb(152, 159, 177)",
+              "fontStyle": "italic"
+            }
+          },
+          {
+            "types": [
+              "string",
+              "builtin",
+              "char",
+              "constant",
+              "url"
+            ],
+            "style": {
+              "color": "rgb(72, 118, 214)"
+            }
+          },
+          {
+            "types": [
+              "variable"
+            ],
+            "style": {
+              "color": "rgb(201, 103, 101)"
+            }
+          },
+          {
+            "types": [
+              "number"
+            ],
+            "style": {
+              "color": "rgb(170, 9, 130)"
+            }
+          },
+          {
+            "types": [
+              "punctuation"
+            ],
+            "style": {
+              "color": "rgb(153, 76, 195)"
+            }
+          },
+          {
+            "types": [
+              "function",
+              "selector",
+              "doctype"
+            ],
+            "style": {
+              "color": "rgb(153, 76, 195)",
+              "fontStyle": "italic"
+            }
+          },
+          {
+            "types": [
+              "class-name"
+            ],
+            "style": {
+              "color": "rgb(17, 17, 17)"
+            }
+          },
+          {
+            "types": [
+              "tag"
+            ],
+            "style": {
+              "color": "rgb(153, 76, 195)"
+            }
+          },
+          {
+            "types": [
+              "operator",
+              "property",
+              "keyword",
+              "namespace"
+            ],
+            "style": {
+              "color": "rgb(12, 150, 155)"
+            }
+          },
+          {
+            "types": [
+              "boolean"
+            ],
+            "style": {
+              "color": "rgb(188, 84, 84)"
+            }
+          }
+        ]
+      },
+      "darkTheme": {
+        "plain": {
+          "color": "#F8F8F2",
+          "backgroundColor": "#282A36"
+        },
+        "styles": [
+          {
+            "types": [
+              "prolog",
+              "constant",
+              "builtin"
+            ],
+            "style": {
+              "color": "rgb(189, 147, 249)"
+            }
+          },
+          {
+            "types": [
+              "inserted",
+              "function"
+            ],
+            "style": {
+              "color": "rgb(80, 250, 123)"
+            }
+          },
+          {
+            "types": [
+              "deleted"
+            ],
+            "style": {
+              "color": "rgb(255, 85, 85)"
+            }
+          },
+          {
+            "types": [
+              "changed"
+            ],
+            "style": {
+              "color": "rgb(255, 184, 108)"
+            }
+          },
+          {
+            "types": [
+              "punctuation",
+              "symbol"
+            ],
+            "style": {
+              "color": "rgb(248, 248, 242)"
+            }
+          },
+          {
+            "types": [
+              "string",
+              "char",
+              "tag",
+              "selector"
+            ],
+            "style": {
+              "color": "rgb(255, 121, 198)"
+            }
+          },
+          {
+            "types": [
+              "keyword",
+              "variable"
+            ],
+            "style": {
+              "color": "rgb(189, 147, 249)",
+              "fontStyle": "italic"
+            }
+          },
+          {
+            "types": [
+              "comment"
+            ],
+            "style": {
+              "color": "rgb(98, 114, 164)"
+            }
+          },
+          {
+            "types": [
+              "attr-name"
+            ],
+            "style": {
+              "color": "rgb(241, 250, 140)"
+            }
+          }
+        ]
+      }
+    },
+    "copyright": "Copyright © 2020-2020 The Monix Connect Developers."
+  },
+  "onBrokenLinks": "throw",
+  "onDuplicateRoutes": "warn",
+  "customFields": {},
+  "plugins": [],
+  "themes": [],
+  "presets": []
+};
